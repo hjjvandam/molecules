0b5de65cf755acdf11bcddd3f0e4d44fe880df4f
braceal <braceal@umich.edu>
2020-01-02 04:01:55 -0500

    updated HyperParams class with load/save functionality

--- molecules/ml/hyperparams.py
+++ molecules/ml/hyperparams.py
@@ -1,6 +1,23 @@
-from abc import ABCMeta
+import pickle
+from abc import ABCMeta, abstractmethod
 
 class HyperParams(metaclass=ABCMeta):
     """Abstract interface for defining hyperparameters"""
     def __init__(self):
         pass
+
+    @abstractmethod
+    def validate(self):
+        raise NotImplementedError('Must implement validate().')
+
+    def save(self, path):
+        """Write HyperParams object to disk."""
+        with open(path, 'wb') as file:
+            pickle.dump(self, file)
+
+    def load(path):
+        """Load HyperParams object from disk."""
+        with open(path, 'rb') as file:
+            hparams = pickle.load(file)
+            hparams.validate()
+            return hparams
