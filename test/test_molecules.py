cca49c41da6caa3ab18d0b34e2daf5d3bddf9f58
braceal <braceal@umich.edu>
2020-01-02 03:59:33 -0500

    added test case for hyperparams load/save functionality

--- test/test_molecules.py
+++ test/test_molecules.py
@@ -1,19 +1,45 @@
 """
 Tests for `molecules` module.
 """
+import os
+import glob
 import pytest
-from molecules import molecules
+from molecules.ml.unsupervised import HyperparamsEncoder
 
 
-class TestMolecules(object):
+class TestHyperParams:
 
     @classmethod
     def setup_class(cls):
-        pass
+        cls.fname = os.path.join('.', 'test', 'data', 'encoder-hparams.pkl')
 
-    def test_something(self):
-        pass
+    def test_save_load_functions(self):
+        # Set model hyperparameters for encoder
+        hparam_options = {'num_conv_layers': 4,
+                          'filters': [64, 64, 64, 64],
+                          'kernels': [3, 3, 3, 3],
+                          'strides': [1, 2, 1, 1],
+                          'num_affine_layers': 1,
+                          'affine_widths': [128],
+                          'latent_dim': 3,
+                          'affine_dropouts': [0]
+                         }
+        encoder_hparams = HyperparamsEncoder(**hparam_options)
+
+        # Save model hyperparameters to disk
+        encoder_hparams.save(self.fname)
+
+        # Check that 'encoder-hparams.pkl' is in ./test/data
+        assert os.path.basename(self.fname) in os.listdir(os.path.dirname(self.fname))
+
+        # Load saved hyperparameters from disk
+        loaded_hparams = HyperparamsEncoder.load(self.fname)
+
+        # Check that all attributes were read from disk correctly
+        for key, val in hparam_options.items():
+            assert getattr(loaded_hparams, key) == val
 
     @classmethod
     def teardown_class(cls):
-        pass
+        # Delete file to clean testing directories
+        os.remove(cls.fname)
