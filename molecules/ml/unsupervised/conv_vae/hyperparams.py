6dcf4079c19cd638b0bd5db23b4e10c618f3ac15
braceal <braceal@umich.edu>
2020-05-07 21:26:15 -0400

    added dropout params to conv decoder

--- molecules/ml/unsupervised/conv_vae/hyperparams.py
+++ molecules/ml/unsupervised/conv_vae/hyperparams.py
@@ -4,7 +4,7 @@ from molecules.ml.hyperparams import Hyperparams
 class ConvVAEHyperparams(Hyperparams):
     def __init__(self, num_conv_layers, filters, kernels,
                  strides, latent_dim, activation, num_affine_layers,
-                 affine_widths):
+                 affine_widths, affine_dropouts):
         super().__init__()
         self.num_conv_layers = num_conv_layers 
         self.filters = filters
@@ -14,6 +14,7 @@ class ConvVAEHyperparams(Hyperparams):
         self.activation = activation
         self.num_affine_layers = num_affine_layers
         self.affine_widths = affine_widths
+        self.affine_dropouts = affine_dropouts
 
     def validate(self):
         if len(self.filters) != self.num_conv_layers:
@@ -24,6 +25,8 @@ class ConvVAEHyperparams(Hyperparams):
             raise Exception('number of strides must equal number of convolutional layers.')
         if len(self.affine_widths) != self.num_affine_layers:
             raise Exception('number of affine width parameters must equal the number of affine layers')
+        if len(self.affine_dropouts) != self.num_affine_layers:
+            raise Exception('number of dropout parameters must equal the number of affine layers')
 
 
 class EncoderHyperparams(ConvVAEHyperparams):
@@ -34,28 +37,21 @@ class EncoderHyperparams(ConvVAEHyperparams):
 
         super().__init__(num_conv_layers, filters, kernels, strides,
                          latent_dim, activation, num_affine_layers,
-                         affine_widths)
-
-        self.affine_dropouts = affine_dropouts
+                         affine_widths, affine_dropouts)
 
         self.validate()
-
-    def validate(self):
-        super().validate()
-        if len(self.affine_dropouts) != self.num_affine_layers:
-            raise Exception('number of dropout parameters must equal the number of affine layers')
 
 
 class DecoderHyperparams(ConvVAEHyperparams):
 
     def __init__(self, num_conv_layers=3, filters=[64, 64, 64], kernels=[3, 3, 3],
                  strides=[1, 2, 1], latent_dim=3, activation='relu', 
-                 num_affine_layers=1, affine_widths=[128],
+                 num_affine_layers=1, affine_widths=[128], affine_dropouts=[0],
                  output_activation='sigmoid'):
 
         super().__init__(num_conv_layers, filters, kernels, strides, 
                          latent_dim, activation, num_affine_layers,
-                         affine_widths)
+                         affine_widths, affine_dropouts)
 
         self.output_activation = output_activation
 
