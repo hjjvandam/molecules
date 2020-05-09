423c647c2a15ff3325f95198078fdfea07efd5c2
braceal <braceal@umich.edu>
2020-05-09 01:57:35 -0400

    handling invalid hparams passed to optimizer

--- molecules/ml/hyperparams/optimizer_hyperparams.py
+++ molecules/ml/hyperparams/optimizer_hyperparams.py
@@ -38,35 +38,41 @@ def get_optimizer(model, hparams):
 
     """
 
-    if hparams.name is 'Adadelta':
-        return optim.Adadelta(model.parameters(), **hparams.hparams)
+    try:
 
-    elif hparams.name is 'Adagrad':
-        return optim.Adagrad(model.parameters(), **hparams.hparams)
+        if hparams.name is 'Adadelta':
+            return optim.Adadelta(model.parameters(), **hparams.hparams)
 
-    elif hparams.name is 'Adam':
-        return optim.Adam(model.parameters(), **hparams.hparams)
+        elif hparams.name is 'Adagrad':
+            return optim.Adagrad(model.parameters(), **hparams.hparams)
 
-    elif hparams.name is 'AdamW':
-        return optim.AdamW(model.parameters(), **hparams.hparams)
+        elif hparams.name is 'Adam':
+            return optim.Adam(model.parameters(), **hparams.hparams)
 
-    elif hparams.name is 'SparseAdam':
-        return optim.SparseAdam(model.parameters(), **hparams.hparams)
+        elif hparams.name is 'AdamW':
+            return optim.AdamW(model.parameters(), **hparams.hparams)
 
-    elif hparams.name is 'Adamax':
-        return optim.Adamax(model.parameters(), **hparams.hparams)
+        elif hparams.name is 'SparseAdam':
+            return optim.SparseAdam(model.parameters(), **hparams.hparams)
 
-    elif hparams.name is 'ASGD':
-        return optim.ASGD(model.parameters(), **hparams.hparams)
+        elif hparams.name is 'Adamax':
+            return optim.Adamax(model.parameters(), **hparams.hparams)
 
-    elif hparams.name is 'LBFGS':
-        return optim.LBFGS(model.parameters(), **hparams.hparams)
+        elif hparams.name is 'ASGD':
+            return optim.ASGD(model.parameters(), **hparams.hparams)
 
-    elif hparams.name is 'RMSprop':
-        return optim.RMSprop(model.parameters(), **hparams.hparams)
+        elif hparams.name is 'LBFGS':
+            return optim.LBFGS(model.parameters(), **hparams.hparams)
 
-    elif hparams.name is 'Rprop':
-        return optim.Rprop(model.parameters(), **hparams.hparams)
+        elif hparams.name is 'RMSprop':
+            return optim.RMSprop(model.parameters(), **hparams.hparams)
 
-    elif hparams.name is 'SGD':
-        return optim.SGD(model.parameters(), **hparams.hparams)
+        elif hparams.name is 'Rprop':
+            return optim.Rprop(model.parameters(), **hparams.hparams)
+
+        elif hparams.name is 'SGD':
+            return optim.SGD(model.parameters(), **hparams.hparams)
+
+    except TypeError as e:
+        raise Exception(f'Invalid parameter in hparams: {hparams.hparams}'
+                        f' for optimizer {hparams.name}.\nSee PyTorch docs.')
