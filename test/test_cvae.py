6604e5b6dc5d263174a2669eadf8d212dc3827d9
braceal <braceal@umich.edu>
2020-05-18 19:50:02 -0400

    added contact map data loader which takes h5 data file path as input

--- test/test_cvae.py
+++ test/test_cvae.py
@@ -9,11 +9,11 @@ from molecules.ml.unsupervised import (VAE, EncoderConvolution2D,
 
 # Pytorch imports
 import torch
+from molecules.utils import open_h5
 from torch.utils.data import Dataset, DataLoader
 from molecules.ml.unsupervised.conv_vae.pytorch_cvae import CVAE
 from molecules.ml.hyperparams import OptimizerHyperparams
 from torchsummary import summary
-
 
 class TestCVAE:
 
@@ -29,6 +29,33 @@ class TestCVAE:
 
             def __getitem__(self, idx):
                 return torch.from_numpy(self.maps[idx]).to(torch.float32)
+
+
+    # TODO: find elegant way to get the input_shape for the model initialization
+    class ContactMap(Dataset):
+        def __init__(self, path, split_ptc=0.8, split='train'):
+            with open_h5(path) as input_file:
+                # Access contact matrix data from h5 file
+                data = np.array(input_file['contact_maps'])
+
+            # 80-20 train validation split index
+            split_ind = int(split_ptc * len(data))
+
+            if split == 'train':
+                self.data = data[:split_ind]
+            elif split == 'valid':
+                self.data = data[split_ind:]
+            else:
+                raise ValueError(f'Parameter split={split} is invalid.')
+
+            self.data = torch.from_numpy(self.data.reshape(-1, 1, 22, 22)).to(torch.float32)
+
+        def __len__(self):
+            return len(self.data)
+
+        def __getitem__(self, idx):
+            return self.data[idx]
+
 
     @classmethod
     def setup_class(self):
@@ -89,7 +116,7 @@ class TestCVAE:
                    batch_size=self.batch_size, epochs=self.epochs)
 
 
-    def test_pytorch_cvae(self):
+    def notest_pytorch_cvae(self):
         cvae = CVAE(self.input_shape, self.encoder_hparams,
                     self.decoder_hparams, self.optimizer_hparams)
 
@@ -108,6 +135,25 @@ class TestCVAE:
 
         assert even_padding(input_dim, kernel_size, stride=1) == 1
         assert even_padding(input_dim, kernel_size, stride=2) == 1
+
+
+    def test_pytorch_cvae_real_data(self):
+
+        path = './test/cvae_input.h5'
+
+        self.train_loader = DataLoader(TestCVAE.ContactMap(path, split='train'),
+                                       batch_size=self.batch_size, shuffle=True)
+        self.test_loader = DataLoader(TestCVAE.ContactMap(path, split='valid'),
+                                      batch_size=self.batch_size, shuffle=True)
+
+        cvae = CVAE(self.input_shape, self.encoder_hparams,
+                    self.decoder_hparams, self.optimizer_hparams)
+
+        print(cvae)
+        summary(cvae.cvae, self.input_shape)
+
+
+        cvae.train(self.train_loader, self.test_loader, self.epochs)
 
     @classmethod
     def teardown_class(self):
