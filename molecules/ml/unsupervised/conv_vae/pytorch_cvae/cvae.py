9d8616dd623dc6f6c3582beca567a405c622b17e
braceal <braceal@umich.edu>
2020-05-18 19:19:15 -0400

    cvae is handling identity matrix data. passing basic tests

--- molecules/ml/unsupervised/conv_vae/pytorch_cvae/cvae.py
+++ molecules/ml/unsupervised/conv_vae/pytorch_cvae/cvae.py
@@ -1,6 +1,7 @@
 import torch
 from torch import nn, optim
 from torch.nn import functional as F
+from math import isclose
 from molecules.ml.hyperparams import OptimizerHyperparams, get_optimizer
 from molecules.ml.unsupervised import EncoderHyperparams, DecoderHyperparams
 
@@ -20,41 +21,45 @@ def conv2d_num_params(f, c, num_filters):
     # +1 to count the bias for each filter
     return (f*f*c + 1) * num_filters
 
-
-def conv2d_output_size(n, f, stride, pad):
+def conv2d_output_dim(input_dim, kernel_size, stride, padding,
+                      transpose=False):
     """
     Parameters
     ----------
-    n : int
+    input_dim : int
         input size. may include padding
     
-    f : int
+    kernel_size : int
         filter size
 
     stride : int
         stride length
 
-    pad : int
+    padding : int
         length of 0 pad
 
     """
-    return (2*pad + n - f) // stride + 1
 
+    if transpose:
+        return (input_dim - 1) * stride + kernel_size - 2*padding
 
-def conv2d_output_shape(n, f, stride, pad, num_filters):
+    return (2*padding + input_dim - kernel_size) // stride + 1
+
+def conv2d_output_shape(input_dim, kernel_size, stride, padding,
+                        num_filters, transpose=False):
     """
     Parameters
     ----------
-    n : int
+    input_dim : int
         input size
     
-    f : int
+    kernel_size : int
         filter size
 
     stride : int
         stride length
 
-    pad : int
+    padding : int
         length of 0 pad
 
     num_filters : int
@@ -66,17 +71,18 @@ def conv2d_output_shape(n, f, stride, pad, num_filters):
     (N,N, num_filters) tuple
 
     """
-    output_size = conv2d_output_size(n, f, stride, pad) 
-    return (output_size, output_size, num_filters)
+    output_dim = conv2d_output_dim(input_dim, kernel_size, stride,
+                                   padding, transpose)
+    return (output_dim, output_dim, num_filters)
 
-def same_padding(input_dim, f, stride):
+def same_padding(input_dim, kernel_size, stride):
     """
     Parameters
     ----------
     n : int
         input size
     
-    f : int
+    kernel_size : int
         filter size
 
     stride : int
@@ -86,11 +92,24 @@ def same_padding(input_dim, f, stride):
     -------
     In this case we want output_dim = input_dim
 
-    input_dim = output_dim = (2*pad + input_dim - f) / stride + 1
+    input_dim = output_dim = (2*pad + input_dim - kernel_size) // stride + 1
 
     Solve for pad. 
     """
-    return (input_dim * (stride - 1) - stride + f) // 2
+    return (input_dim * (stride - 1) - stride + kernel_size) // 2
+
+def even_padding(input_dim, kernel_size, stride):
+    """
+    Implements Keras-like padding.
+    If the stride is one then use same_padding.
+    Otherwise, select the smallest pad such that the
+    kernel_size fits evenly within the input_dim.
+    """
+    if stride == 1:
+        return same_padding(input_dim, kernel_size, stride)
+
+    # TODO: this could have bugs for stride != 1. Needs testing.
+    return same_padding(input_dim // stride, kernel_size, 1)
 
 
 # TODO: conider moving this into the HyperParams class
@@ -126,6 +145,7 @@ class EncoderConv2D(nn.Module):
         # Assume input is square matrix
         self.input_shape = input_shape
         self.hparams = hparams
+        self.encoder_dim = input_shape[1]
 
         self.encoder = nn.Sequential(*self._conv_layers(),
                                      nn.Flatten(),
@@ -151,13 +171,13 @@ class EncoderConv2D(nn.Module):
         conv2d_layers = []
 
         # Contact matrices have one channel
-        in_channels = 1
+        in_channels = self.input_shape[0]
 
         for filter_, kernel, stride in zip(self.hparams.filters,
                                            self.hparams.kernels,
                                            self.hparams.strides):
 
-            padding = same_padding(self.input_shape[0], kernel, stride)
+            padding = even_padding(self.encoder_dim, kernel, stride)
 
             conv2d_layers.append(nn.Conv2d(in_channels=in_channels,
                                            out_channels=filter_,
@@ -169,6 +189,8 @@ class EncoderConv2D(nn.Module):
 
             # Subsequent layers in_channels is the current layers number of filters
             in_channels = filter_
+
+            self.encoder_dim = conv2d_output_dim(self.encoder_dim, kernel, stride, padding)
 
         return conv2d_layers
 
@@ -185,7 +207,7 @@ class EncoderConv2D(nn.Module):
         fc_layers = []
 
         # First layer gets flattened convolutional output
-        in_features = self.input_shape[0]**2 * self.hparams.filters[-1]
+        in_features = self.hparams.filters[-1] * self.encoder_dim**2
 
         for width, dropout in zip(self.hparams.affine_widths,
                                   self.hparams.affine_dropouts):
@@ -195,7 +217,8 @@ class EncoderConv2D(nn.Module):
 
             fc_layers.append(select_activation(self.hparams.activation))
 
-            fc_layers.append(nn.Dropout(p=dropout))
+            if not isclose(dropout, 0):
+                fc_layers.append(nn.Dropout(p=dropout))
 
             # Subsequent layers in_features is the current layers width
             in_features = width
@@ -208,13 +231,14 @@ class EncoderConv2D(nn.Module):
 
 
 class DecoderConv2D(nn.Module):
-    def __init__(self, output_shape, hparams):
+    def __init__(self, output_shape, encoder_dim, hparams):
         super(DecoderConv2D, self).__init__()
 
         hparams.validate()
 
         # Assume input is square matrix
         self.output_shape = output_shape
+        self.encoder_dim = encoder_dim
         self.hparams = hparams
 
         self.affine_layers = nn.Sequential(*self._affine_layers())
@@ -222,19 +246,16 @@ class DecoderConv2D(nn.Module):
 
     def reshape(self, x):
         """
-        Reshape flattened x as a tensor (output, output, remainder)
+        Reshape flattened x as a tensor (channels, output, output)
         """
-        new_shape = (*self.output_shape, self.hparams.affine_widths[-1] / self.output_shape[0]**2)
+        new_shape = (-1, self.hparams.filters[0],
+                     self.encoder_dim, self.encoder_dim)
         return x.view(new_shape)
 
     def forward(self, x):
         x = self.affine_layers(x)
         x = self.reshape(x)
         return self.conv_layers(x)
-
-
-    # TODO: _conv_layers could have a bug in the in_channels. Needs testing.
-    #       Check that the output dimension is correct
 
     def _conv_layers(self):
         """
@@ -247,28 +268,37 @@ class DecoderConv2D(nn.Module):
         """
         conv2d_layers = []
 
-        print('PRINT')
-        print(self.hparams.affine_widths[-1])
-        print(self.output_shape[0]**2)
+        in_channels = self.hparams.filters[0]
 
-        in_channels = self.hparams.affine_widths[-1]
+        # Dimension of square matrix
+        input_dim = self.output_shape[1]
+
+        # Set last filter to be the number of channels in the reconstructed image.
+        self.hparams.filters[-1] = self.output_shape[0]
 
         for filter_, kernel, stride in zip(self.hparams.filters,
                                            self.hparams.kernels,
                                            self.hparams.strides):
 
-            padding = same_padding(self.output_shape[0], kernel, stride)
+            padding = even_padding(input_dim, kernel, stride)
 
-            conv2d_layers.append(nn.Conv2d(in_channels=in_channels,
-                                           out_channels=filter_,
-                                           kernel_size=kernel,
-                                           stride=stride,
-                                           padding=padding))
+            conv2d_layers.append(nn.ConvTranspose2d(in_channels=in_channels,
+                                                    out_channels=filter_,
+                                                    kernel_size=kernel,
+                                                    stride=stride,
+                                                    padding=padding,
+                                                    output_padding=1 if stride != 1 else 0))
+
+            # TODO: revist output_padding. This code may not generalize to other examples. Needs testing.
 
             conv2d_layers.append(select_activation(self.hparams.activation))
 
             # Subsequent layers in_channels is the current layers number of filters
+            # Except for the last layer which is 1 (or output_shape channels)
             in_channels = filter_
+
+            # Compute non-channel dimension given to next layer
+            input_dim = conv2d_output_dim(input_dim, kernel, stride, padding, transpose=True)
 
         # Overwrite output activation
         conv2d_layers[-1] = select_activation(self.hparams.output_activation)
@@ -297,14 +327,20 @@ class DecoderConv2D(nn.Module):
 
             fc_layers.append(select_activation(self.hparams.activation))
 
-            fc_layers.append(nn.Dropout(p=dropout))
+            if not isclose(dropout, 0):
+                fc_layers.append(nn.Dropout(p=dropout))
 
             # Subsequent layers in_features is the current layers width
             in_features = width
 
+        # Add last layer with dims to connect the last linear layer to
+        # the first convolutional decoder layer
+        fc_layers.append(nn.Linear(in_features=self.hparams.affine_widths[-1],
+                                   out_features=self.hparams.filters[0] * self.encoder_dim**2))
+        fc_layers.append(select_activation(self.hparams.activation))
+
+
         return fc_layers
-
-
 
 # Helpful links:
 #   https://github.com/L1aoXingyu/pytorch-beginner/blob/master/08-AutoEncoder/conv_autoencoder.py
@@ -318,7 +354,8 @@ class CVAEModel(nn.Module):
 
         # May not need these .to(device) since it is called in CVAE. Test this
         self.encoder = EncoderConv2D(input_shape, encoder_hparams).to(device)
-        self.decoder = DecoderConv2D(input_shape, decoder_hparams).to(device)
+        self.decoder = DecoderConv2D(input_shape, self.encoder.encoder_dim,
+                                     decoder_hparams).to(device)
 
     def reparameterize(self, mu, logvar):
         std = torch.exp(0.5*logvar)
@@ -327,8 +364,13 @@ class CVAEModel(nn.Module):
 
     def forward(self, x):
         mu, logvar = self.encoder(x)
-        x = self.reparameterize(x, mu, logvar)
-        return self.decoder(x), mu, logvar
+        x = self.reparameterize(mu, logvar)
+        x = self.decoder(x)
+        # TODO: having issue with nans in BCE loss function. Try weight initialization.
+        x = torch.where(torch.isnan(x), torch.zeros_like(x), x)
+        return x, mu, logvar
+
+    # TODO: wrap embed, generate in context manager with no_grad() ?
 
     def embed(self, data):
         # mu layer
@@ -347,6 +389,7 @@ class CVAEModel(nn.Module):
 
 
 class CVAE:
+    # TODO: set weight initialization hparams
     def __init__(self, input_shape,
                  encoder_hparams=EncoderHyperparams(),
                  decoder_hparams=DecoderHyperparams(),
@@ -356,10 +399,6 @@ class CVAE:
         optimizer_hparams.validate()
 
         self.input_shape = input_shape
-        self.optimizer_hparams = optimizer_hparams
-
-        # Defined in self.train()
-        self.optimizer = None
 
         self.loss = self._loss if loss is None else loss
 
@@ -369,17 +408,21 @@ class CVAE:
         self.cvae = CVAEModel(input_shape, encoder_hparams,
                               decoder_hparams, self.device).to(self.device)
 
-        print(self.cvae)
+        # TODO: consider making optimizer_hparams a member variable
+        # RMSprop with lr=0.001, alpha=0.9, epsilon=1e-08, decay=0.0
+        self.optimizer = get_optimizer(self.cvae, optimizer_hparams)
+
+    def __repr__(self):
+        return str(self.cvae)
 
     def train(self, train_loader, test_loader, epochs):
-        # RMSprop with lr=0.001, alpha=0.9, epsilon=1e-08, decay=0.0
-        self.optimizer = get_optimizer(self.cvae, self.optimizer_hparams)
         for epoch in range(1, epochs + 1):
             self._train(train_loader, epoch)
             self._test(test_loader)
 
     def _train(self, train_loader, epoch,
-               checkpoint=None, callbacks=None):
+               checkpoint=None, callbacks=None,
+               log_interval=2):
         """
         Effects
         -------
@@ -420,7 +463,7 @@ class CVAE:
 
 
             # TODO: Handle logging/checkpoint
-            if batch_idx % args.log_interval == 0:
+            if batch_idx % log_interval == 0:
                 print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                     epoch, batch_idx * len(data), len(train_loader.dataset),
                     100. * batch_idx / len(train_loader),
@@ -454,7 +497,7 @@ class CVAE:
         https://arxiv.org/abs/1312.6114
 
         """
-        BCE = F.binary_cross_entropy(recon_x, x.view(-1, self.input_shape[0]**2), reduction='sum')
+        BCE = F.binary_cross_entropy(recon_x, x, reduction='sum')
 
         # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
         KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
