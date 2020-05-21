5985a94a3acc3c26fa92e320d4cee2d7e96436ea
braceal <braceal@umich.edu>
2020-05-21 03:43:44 -0400

    nearly complete encoder. debugging output shape of residual layers

--- molecules/ml/unsupervised/vae/resnet/encoder.py
+++ molecules/ml/unsupervised/vae/resnet/encoder.py
@@ -1,9 +1,14 @@
 import torch
 from torch import nn
-from molecules.ml.unsupervised.vae.utils import (conv2d_output_dim, same_padding,
-                                                 select_activation, init_weights)
+from molecules.ml.unsupervised.vae.utils import (conv_output_dim, conv_output_shape,
+                                                 same_padding, select_activation,
+                                                 init_weights)
 from molecules.ml.unsupervised.vae.resnet import ResnetVAEHyperparams
 from molecules.ml.unsupervised.vae.resnet.residual_module import ResidualConv1d
+
+# Helper function to return product of elements in a tuple
+from functools import reduce
+prod = lambda tup: reduce((lambda x, y: x * y), tup)
 
 class ResnetEncoder(nn.Module):
     def __init__(self, input_shape, hparams):
@@ -16,10 +21,10 @@ class ResnetEncoder(nn.Module):
         self.input_shape = input_shape
         self.hparams = hparams
 
-        self.encoder = nn.Sequential(*self._encoder_layers())
+        self.encoder, output_shape = self._encoder_layers()
 
-        self.mu = self._embedding_layer()
-        self.logvar = self._embedding_layer()
+        self.mu = self._embedding_layer(output_shape)
+        self.logvar = self._embedding_layer(output_shape)
 
         self.init_weights()
 
@@ -29,7 +34,10 @@ class ResnetEncoder(nn.Module):
         init_weights(self.logvar)
 
     def forward(self, x):
-        x = self.encoder(x)
+        print('forward before: ',x.shape)
+        # TODO: reshape in dataloader during preprocessing. Add flatten option to dataset class
+        x = self.encoder(x.view(-1, 1, self.input_shape[1]**2))
+        print('forward: ',x.shape)
         return self.mu(x), self.logvar(x)
 
     def encode(self, x):
@@ -46,12 +54,9 @@ class ResnetEncoder(nn.Module):
     def _encoder_layers(self):
         layers = []
 
-        # Contact matrices have one channel
-        in_channels = self.input_shape[0]
-
         padding = same_padding(self.input_shape[1], kernel_size=5, stride=1)
 
-        layers.append(nn.Conv1d(in_channels=in_channels,
+        layers.append(nn.Conv1d(in_channels=self.input_shape[0], # should be 1
                                 out_channels=self.hparams.enc_filters,
                                 kernel_size=5,
                                 stride=1,
@@ -59,9 +64,28 @@ class ResnetEncoder(nn.Module):
 
         layers.append(select_activation(self.hparams.activation))
 
-        return layers
+        res_input_shape = conv_output_shape(self.input_shape[1], kernel_size=5,
+                                            stride=1, padding=padding,
+                                            num_filters=self.hparams.enc_filters, dim=1)
 
-    def _embedding_layer(self):
-        # TODO: implement
-        return nn.Linear(in_features=1,
-                         out_features=1)
+        # Add residual layers
+        for lidx in range(self.hparams.enc_reslayers):
+
+            filters = self.hparams.enc_filters * self.hparams.enc_filter_growth_fac**lidx
+            filters = round(filters) # To nearest int
+
+            layers.append(ResidualConv1d(res_input_shape,
+                                         filters,
+                                         self.hparams.kernel_size,
+                                         self.hparams.activation,
+                                         shrink=True))
+
+            res_input_shape = layers[-1].shape
+
+        return nn.Sequential(*layers, nn.Flatten()), res_input_shape
+
+    def _embedding_layer(self, output_shape):
+        print(output_shape)
+        print(prod(output_shape))
+        return nn.Linear(in_features=prod(output_shape),
+                         out_features=self.hparams.latent_dim)
