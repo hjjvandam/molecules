2c753cdbc9942ce7b44bd88ef7606d035557363a
braceal <braceal@umich.edu>
2020-06-09 23:54:21 -0400

    added rmsd and fraction of native contacts to reporter

--- molecules/sim/openmm_reporter.py
+++ molecules/sim/openmm_reporter.py
@@ -1,8 +1,8 @@
 import h5py 
 import numpy as np
 import simtk.unit as u
-from MDAnalysis.analysis import distances
-
+from MDAnalysis import Universe
+from MDAnalysis.analysis import distances, rms
 
 # TODO: h5py help https://www.pythonforthelab.com/blog/how-to-use-hdf5-files-in-python/
 
@@ -12,10 +12,12 @@ class ContactMapReporter:
         self._file = h5py.File(file, 'w', libver='latest', swmr=True)
         #self._file.swmr_mode = True
         self._cm_dset = self._file.create_dataset('contact_maps', shape=(2,0), maxshape=(None, None))
-        self._rmsd_dset = self._file.create_dataset('rmsd', shape=(1,), maxshape=(None,))
-        self._native_contact_dset = self._file.create_dataset('native_contact', shape=(1,), maxshape=(None,))
+        self._rmsd_dset = self._file.create_dataset('rmsd', shape=(1,1), maxshape=(None,1))
+        self._native_contact_dset = self._file.create_dataset('native_contact', shape=(1,1), maxshape=(None,1))
 
-        # TODO: load in native pdb into a mdanalysis universe
+        u = Universe(native_pdb)
+        self._native_positions = u.select_atoms('protein').positions()
+        self._native_ca_positions = u.select_atoms('CA').positions()
 
     def __del__(self):
         self._file.close()
@@ -24,27 +26,33 @@ class ContactMapReporter:
         steps = self._report_interval - simulation.currentStep % self._report_interval
         return (steps, True, False, False, False, None)
 
-    def _report_contact_maps(self, simulation, state):
+    def _report_contact_maps(self, simulation, state, ca_positions):
         # TODO: can we store contact_map dtype as bool?
-        ca_indices = [atom.index for atom in simulation.topology.atoms() if atom.name == 'CA']
-        positions = np.array(state.getPositions().value_in_unit(u.angstrom))
-        positions_ca = positions[ca_indices].astype(np.float32)
-        distance_matrix = distances.self_distance_array(positions_ca)
-        contact_map = (distance_matrix < 8.0) * 1.0 
-        new_shape = (len(contact_map), self._cm_dset.shape[1] + 1)
-        self._cm_dset.resize(new_shape)
-        self._cm_dset[:, new_shape[1] - 1] = contact_map
+        contact_map = (distances.self_distance_array(ca_positions) < 8.) * 1.
 
-    def _report_rmsd(self, simulation, state):
-        # TODO: which atoms do we compute rmsd for
-        pass
+        self._cm_dset.resize((len(contact_map), self._cm_dset.shape[1] + 1))
+        self._cm_dset[:, -1] = contact_map
 
-    def _report_native_contact(self, simulation, state):
-        # TODO: which atoms do we compute rmsd for
-        pass
+    def _report_rmsd(self, simulation, state, positions):
+        # RMSD of all protein atoms
+        rmsd = rms.rmsd(positions, self._native_positions)
+
+        self._rmsd_dset.resize(1, self._rmsd_dset.shape[1] + 1)
+        self._rmsd_dset[0, -1] = rmsd
+
+    def _report_native_contact(self, simulation, state, ca_positions):
+        # Native contacts are based on a distance cutoff of 8 Ansgtroms less between between C-alphas
+        native_contact = (np.abs(ca_positions - self._native_ca_positions) < 8.).mean()
+
+        self._native_contact_dset.resize(1, self._native_contact_dset.shape[1] + 1)
+        self._native_contact_dset[0, -1] = native_contact
 
     def _report(self, simulation, state):
-        self._report_contact_maps(simulation, state)
-        self._report_rmsd(simulation, state)
-        self._report_native_contact(simulation, state)
+        ca_indices = [a.index for a in simulation.topology.atoms() if a.name == 'CA']
+        positions = np.array(state.getPositions().value_in_unit(u.angstrom))
+        ca_positions = positions[ca_indices].astype(np.float32)
+
+        self._report_contact_maps(simulation, state, ca_positions)
+        self._report_rmsd(simulation, state, positions)
+        self._report_native_contact(simulation, state, ca_positions)
         self._file.flush()
