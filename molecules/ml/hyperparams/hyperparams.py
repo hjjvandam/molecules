6984261f9541af0fe6cd66705e2e02ea4e4c2daf
braceal <braceal@umich.edu>
2020-07-15 13:05:22 -0400

    added hparams_type to json

--- molecules/ml/hyperparams/hyperparams.py
+++ molecules/ml/hyperparams/hyperparams.py
@@ -5,6 +5,10 @@ class Hyperparams(metaclass=ABCMeta):
     """Abstract interface for defining hyperparameters"""
     def __init__(self):
         self.validate()
+        if 'hparam_type' in self.__dict__:
+            raise ValueError("'hparam_type' is not allowed to be used as "
+                             "a member variable name since it is used "
+                             "to save models to a file.")
 
     def __repr__(self):
         return f'{self.__class__.__name__}\n' + \
@@ -17,11 +21,15 @@ class Hyperparams(metaclass=ABCMeta):
     def save(self, path):
         """Write HyperParams object to disk."""
         with open(path, 'w') as file:
+            payload = self.__dict__
+            payload['hparam_type'] = self.__class__.__name__
             json.dump(self.__dict__, file)
 
     def load(self, path):
         """Load HyperParams object from disk."""
         with open(path, 'r') as file:
-            self.__dict__ = json.load(file)
+            hparams = json.load(file)
+            hparams.pop('hparam_type')
+            self.__dict__ = hparams
         self.validate()
         return self
