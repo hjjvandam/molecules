f3891c8705008737c1f2b1f8f466b5afd91a5871
braceal <braceal@umich.edu>
2020-08-07 19:19:08 -0700

    allowed for many dcd files

--- scripts/traj_to_sparse_contact_map.py
+++ scripts/traj_to_sparse_contact_map.py
@@ -1,6 +1,6 @@
+import os
+import glob
 import click
-import numpy as np
-from molecules.utils import open_h5
 from molecules.sim.contact_maps import sparse_contact_maps_from_traj
 
 @click.command()
@@ -15,7 +15,10 @@ from molecules.sim.contact_maps import sparse_contact_maps_from_traj
 
 @click.option('-t', 'traj_path', required=True,
               type=click.Path(exists=True),
-              help='Path to file containing MD trajectory (DCD)')
+              help='Path to file containing MD trajectory (DCD) ' \
+                   'OR directory containing many .dcd files. If ' \
+                   'directory, files are sorted lexographically ' \
+                   'by their names and then concatenated.')
 
 @click.option('-o', 'out_path', required=True,
               help='Path to file to write sparse contact matrices to')
@@ -23,6 +26,9 @@ from molecules.sim.contact_maps import sparse_contact_maps_from_traj
 @click.option('-v', '--verbose', is_flag=True)
 
 def main(pdb_path, ref_pdb_path, traj_path, out_path, verbose):
+
+    if os.path.isdir(traj_path):
+        traj_path = sorted(glob.glob(os.path.join(traj_path, '*.dcd')))
 
     sparse_contact_maps_from_traj(pdb_path, ref_pdb_path,
                                   traj_path, save_file=out_path,
