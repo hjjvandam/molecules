2d2518ae0560d11a42ce745acc0c0431c419ebc5
braceal <braceal@umich.edu>
2020-08-19 20:36:28 -0700

    dset concat tested. works.

--- scripts/concat_dsets.py
+++ scripts/concat_dsets.py
@@ -21,25 +21,46 @@ class PythonLiteralOption(click.Option):
               type=click.Path(exists=True),
               help='Directory path containing individual h5 files.')
 
-@click.option('-p', '--pattern', default='',
+@click.option('-p', '--pattern', default='*.h5',
               help='File name pattern to glob particular h5 files. ' \
                    'Wildcards allowed. EX: *_traj[0-10].h5')
 
 @click.option('-o', '--out_path', required=True,
               help='Path to file to write concatenated dataset to.')
 
-@click.option('-f', '--fields', cls=PythonLiteralOption,
-              default=["contact_map", "point_cloud", "rmsd", "fnc"],
-              help='A Python list containing the data field names. ' \
-                   'EX: ["contact_map", "point_cloud", "rmsd", "fnc"]')
+@click.option('--rmsd', is_flag=True,
+              help='Concatenates RMSD data field in new dataset.')
+
+@click.option('--fnc', is_flag=True,
+              help='Concatenates fraction of reference contacts data field in new dataset.')
+
+@click.option('--contact_map', is_flag=True,
+              help='Concatenates contact maps data field in new dataset. ' \
+                   'Note: only works for sparse-concat formatted contact maps.')
+
+@click.option('--point_cloud', is_flag=True,
+              help='Concatenates point cloud data field in new dataset.')
 
 @click.option('-v', '--verbose', is_flag=True)
 
-def main(data_root, out_path, fields, verbose):
+def main(data_root, pattern, out_path, rmsd, fnc, contact_map, point_cloud, verbose):
+
+    fields = []
+    if rmsd: fields.append('rmsd')
+    if fnc: fields.append('fnc')
+    if contact_map: fields.append('contact_map')
+    if point_cloud: fields.append('point_cloud')
+
+    if not fields:
+        raise ValueError('No data fields selected to concatenate. Add any ' \
+                         ' combination of --rmsd --fnc --contact_map --point_cloud')
 
     # Get list of input h5 files
     files = sorted(glob.glob(os.path.join(data_root, pattern)))
-    files = filter(lambda x: x != out_path, files)
+    files = list(filter(lambda x: x != out_path, files))
+
+    if verbose:
+        print(f'Collected {len(files)} h5 files.')
 
     # Open output file
     fout = h5py.File(out_path, 'w', libver='latest')
@@ -62,7 +83,8 @@ def main(data_root, out_path, fields, verbose):
 
     # Centor of mass (CMS) subtraction
     if 'point_cloud' in data:
-        print('Subtract center of mass (CMS) from point cloud')
+        if verbose:
+            print('Subtract center of mass (CMS) from point cloud')
         cms = np.mean(data['point_cloud'][:, 0:3, :].astype(np.float128), axis=2, keepdims=True).astype(np.float32)
         data['point_cloud'][:, 0:3, :] -= cms
 
