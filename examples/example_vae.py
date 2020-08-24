4ab7506a4bbd3893a6c72fc642a3dbd5b4d62155
Awesom E. Dev <devnull@nvidia.com>
2020-08-24 15:30:06 -0700

    generalized model

--- examples/example_vae.py
+++ examples/example_vae.py
@@ -138,7 +138,7 @@ def main(input_path, out_path, checkpoint, model_id, dim1, dim2, cm_format, enco
                           'nchars': dim2,
                           'latent_dim': latent_dim,
                           'dec_filters': dim1,
-                          'enc_reslayers': 10,
+                          'enc_reslayers': 6,
                           'output_activation': 'None'}
 
         input_shape = (dim1, dim1)
