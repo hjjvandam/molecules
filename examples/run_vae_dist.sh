75c288109b939c5958dffe09d69b97ff93a3794d
Awesom E. Dev <devnull@nvidia.com>
2020-08-25 19:14:39 -0700

    extended run script to allow for CP restart

--- examples/run_vae_dist.sh
+++ examples/run_vae_dist.sh
@@ -3,6 +3,12 @@
 # run tag
 run_tag=${2}
 output_dir="/runs/${run_tag}"
+checkpoint=${3}
+
+cflag=""
+if [ ! -z "${checkpoint}" ]; then
+    cflag="-c ${checkpoint}"
+fi
 
 # connect to wandb
 wandb login ${1}
@@ -17,13 +23,13 @@ dec_gpu=$(( 2 * ${LOCAL_RANK} + 1 ))
 # launch code
 python ./example_vae.py \
        -i "/data/spike-full-point-cloud_closed.h5" \
-       -o ${output_dir} -m ${run_tag} \
+       -o ${output_dir} -m ${run_tag} ${cflag} \
        --wandb_project_name covid_dl \
        --amp --distributed \
        -f sparse-concat \
        -t resnet \
        -e 150 \
-       -b 16 \
+       -b 12 \
        -E ${enc_gpu} -D ${dec_gpu} \
        -S 3 \
        -h 3768 -w 3768 -d 256
