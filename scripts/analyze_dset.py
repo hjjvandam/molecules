23032b4e4060d0fa19250c0239926f9f7e9cf6fa
braceal <braceal@umich.edu>
2020-09-20 16:27:18 -0700

    added script to report sparsity of dataset

---
+++ scripts/analyze_dset.py
@@ -0,0 +1,51 @@
+import click
+
+@click.command()
+@click.option('-i', '--input', 'input_path', required=True,
+              type=click.Path(exists=True),
+              help='Path to file containing preprocessed contact matrix data')
+
+@click.option('-h', '--dim1', required=True, type=int,
+              help='H of (H,W) shaped contact matrix')
+
+@click.option('-w', '--dim2', required=True, type=int,
+              help='W of (H,W) shaped contact matrix')
+
+@click.option('-f', '--cm_format', default='sparse-concat',
+              help='Format of contact map files. Options ' \
+                   '[full, sparse-concat, sparse-rowcol]')
+
+@click.option('-dn', '--dataset_name', default='contact_map',
+              help='Name of the dataset in the HDF5 file.')
+
+def main(input_path, dim1, dim2, cm_format, dataset_name):
+    import h5py as h5
+    import numpy as np
+
+    nelem = []
+    f = h5.File(input_path, 'r')
+    if cm_format == 'sparse-concat':
+        for item in f[dataset_name]:
+            # Counts number of 1s since each contact map stores row,col positions
+            nelem.append(len(item) / 2)
+    elif cm_format == 'sparse-rowcol':
+        print(f['rmsd'][...])
+        rows = f[dataset_name]['col']
+        print(rows)
+        for i in range(len(rows)):
+            print(rows[i, ...])
+            nelem.append(len(row))
+    elif cm_format == 'full':
+        for item in f[dataset_name][...]:
+            nelem.append(np.sum(item))
+
+    sparsity = np.array(nelem) / (dim1 * dim2)
+    
+    print(f'Contacts: {np.mean(nelem)} +- {np.std(nelem)}')
+    print(f'Sparsity: {np.mean(sparsity)} +- {np.std(sparsity)}')
+    
+    f.close()
+
+
+if __name__ == "__main__":
+    main()
